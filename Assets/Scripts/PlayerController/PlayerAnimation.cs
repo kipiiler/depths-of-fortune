ddc3cd2f2090b8c33933ae68727359976b902e42
Darel <darelg2@uw.edu>
2024-05-28 23:34:07 -0700

    player animations

---
+++ Assets/Scripts/PlayerController/PlayerAnimation.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Animator anim;
+    private FirstPersonController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<FirstPersonController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("Walking", controller.isWalking);
+        anim.SetBool("Sprinting", controller.isSprinting);
+        anim.SetBool("Grounded", controller.isGrounded);
+        anim.SetFloat("vy", controller.velocity.y);
+        anim.SetBool("Crouched", controller.isCrouched);
+        anim.SetBool("HasTorch", controller.hasTorch);
+        anim.SetBool("Attack", controller.isAttacking);
+    }
+}
---
+++ Assets/Scripts/PlayerController/PlayerSound.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSound : MonoBehaviour
+{
+    private FirstPersonController controller;
+    public GameObject walkFootstep;
+    public GameObject runFootstep;
+    
+    private void Awake()
+    {
+        walkFootstep.SetActive(false);
+        runFootstep.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<FirstPersonController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (controller.isGrounded)
+        {
+            if (controller.isCrouched)
+            {
+                walkFootstep.SetActive(false);
+                runFootstep.SetActive(false);
+            }
+            else if (controller.isSprinting)
+            {
+                walkFootstep.SetActive(false);
+                runFootstep.SetActive(true);
+                Sound runSound = new Sound(transform.position, 25f);
+                Sounds.MakeSound(runSound);
+            }
+            else if (controller.isWalking)
+            {
+                walkFootstep.SetActive(true);
+                runFootstep.SetActive(false);
+                Sound walkSound = new Sound(transform.position, 10f);
+                Sounds.MakeSound(walkSound);
+            }
+            else
+            {
+                walkFootstep.SetActive(false);
+                runFootstep.SetActive(false);
+            }
+        }
+        else
+        {
+            walkFootstep.SetActive(false);
+            runFootstep.SetActive(false);
+        }
+    }
+}
