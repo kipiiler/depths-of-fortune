6afb85c8e812f456cb08cd5951f049538448ecbe
hoanghohohaha <hoangng@cs.washington.edu>
2024-06-04 21:56:55 -0700

    clean up

--- Assets/Scripts/SoundSystem/MonsterHearing.cs
+++ Assets/Scripts/SoundSystem/MonsterHearing.cs
@@ -7,17 +7,17 @@ public class MonsterHearing : MonoBehaviour, IHear
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void RespondToSound(Sound sound)
     {
-        Debug.Log("I hear sound from " + sound.pos + " with intensity " + sound.intensity);
+        // Debug.Log("I hear sound from " + sound.pos + " with intensity " + sound.intensity);
     }
 }
